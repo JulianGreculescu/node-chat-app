03ac394e1724fb97fdbd60c4addbe96eb9b6c86d
Julian Greculescu <julian.greculescu@gmail.com>
2017-04-13 09:12:26 +1000

    Add style to the chat page

--- public/js/index.js
+++ public/js/index.js
@@ -30,11 +30,12 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextBox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function () {
-
+    messageTextBox.val('');
   });
 });
 
@@ -44,7 +45,9 @@ locationButton.on('click', function () {
     return alert('Geolocation is not supported by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location ...');
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       longitude: position.coords.longitude,
       latitude: position.coords.latitude
@@ -52,6 +55,7 @@ locationButton.on('click', function () {
     console.log(position);
   },
   function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch the location');
   })
 });
